e4ac383693fb02c8eb969ec3ce7fef1c6a22be44
Angel Chica <4ng3l@Angels-MacBook-Air.local>
2025-10-03 18:06:13 -0400

    started styling

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -1,86 +1,52 @@
+import { ListGroup, ListGroupItem } from "react-bootstrap";
+import ModulesControls from "./ModulesControls";
+import LessonControlButtons from "./LessonControlButtons";
+import ModuleControlButtons from "./ModuleControlButtons";
+import { BsGripVertical } from "react-icons/bs";
+
 export default function Modules() {
   return (
     <div>
-      {/* Implement Collapse All button, View Progress button, etc. */}
-      <button id="wd-collapsebtn"> Collapse All </button>
-      <button id="wd-viewprogbtn"> View Progress </button>
-      <select id = "wd-nodeselc" defaultValue={"PUBLISH-ALL"}>
-        <option value={"PUBLISH"}> Publish All </option>
-      </select>
-      <button id="wd-addmodulebtn"> + Module </button>
-      <ul id="wd-modules">
-        <li className="wd-module">
-          <div className="wd-title">Week 1, Lecture 1 - Course Intro, Syllabus, Agenda</div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Introduction to the course</li>
-                <li className="wd-content-item">Learn what is Web Development</li>
-              </ul>
-            </li>
-            <li>
-              <span className="wd-title">READING</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Full-Stack Developer - Chapter 1 - Introduction</li>
-                <li className="wd-content-item">Full-Stack Developer - Chapter 2 - Creating User Interfaces with HTML</li>
-              </ul>
-            </li>
-            <li>
-              <span className="wd-title">SLIDES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Introduction to Web Development</li>
-                <li className="wd-content-item">Creating an HTTP server with Node.js</li>
-                <li className="wd-content-item">Creating a React Application</li>
-              </ul>
-            </li>
-          </ul>
-        </li> <br />
-        <li className="wd-module">
-          <div className="wd-title">Week 2, Lecture 2 - Formating User Interfaces with HTML</div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Learn how to create user interfaces with HTML</li>
-                <li className="wd-content-item">Deploy the assignment to Nelify</li>
-              </ul>
-            </li>
-            <li>
-              <span className="wd-title">READING</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Full-Stack Developer - Chapter 3 - Formating with CSS</li>
-                <li className="wd-content-item">Full-Stack Developer - Chapter 4 - Making templates</li>
-              </ul>
-            </li>
-            <li>
-              <span className="wd-title">SLIDES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Introduction to user interface development</li>
-                <li className="wd-content-item">Deploying to Nelify</li>
-              </ul>
-            </li>
-          </ul>
-        </li> <br />
-        <li className="wd-module">
-          <div className="wd-title">Week 3, Lecture 3 - Styling pages with CSS</div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Learn what CSS is</li>
-                <li className="wd-content-item">Learn how to make your own stylesheets</li>
-              </ul>
-            </li>
-            <li>
-              <span className="wd-title">SLIDES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Introduction to CSS</li>
-                <li className="wd-content-item">Custom Stylesheets using CSS</li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-      </ul>
+      <ModulesControls /><br /><br /><br /><br />
+      <ListGroup className="rounded-0" id="wd-modules">
+        <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary"> 
+            <BsGripVertical className="me-2 fs-3" />Week 1 <ModuleControlButtons/>
+          </div>
+          <ListGroup className="wd-lessons rounded-0">
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3"/> LEARNING OBJECTIVES <LessonControlButtons/> 
+            </ListGroupItem>
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3"/> Introduction to the course <LessonControlButtons/>
+            </ListGroupItem>
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3"/> Learn what is Web Development <LessonControlButtons/>
+            </ListGroupItem>
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3"/> LESSON 1 <LessonControlButtons/>
+            </ListGroupItem> 
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3"/> LESSON 2 <LessonControlButtons/>
+            </ListGroupItem> 
+          </ListGroup>
+        </ListGroupItem>
+        <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" /> Week 2 <ModuleControlButtons/>
+          </div>
+          <ListGroup className="wd-lessons rounded-0">
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3"/> LEARNING OBJECTIVES <LessonControlButtons/> 
+            </ListGroupItem> 
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3"/> LESSON 1 <LessonControlButtons/> 
+            </ListGroupItem>
+            <ListGroupItem className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3"/> LESSON 2 <LessonControlButtons/> 
+            </ListGroupItem>
+          </ListGroup>
+        </ListGroupItem>
+      </ListGroup>
     </div>
 );}
--- app/(Kambaz)/Navigation.tsx
+++ app/(Kambaz)/Navigation.tsx
@@ -1,13 +1,91 @@
+"use client";
+
+import { AiOutlineDashboard } from "react-icons/ai";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function KambazNavigation() {
+  const pathname = usePathname();
+  const isActive = (p: string) =>
+    pathname === p || pathname.startsWith(p + "/");
+
   return (
-    <div id="wd-kambaz-navigation">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
-      <Link href="/Account" id="wd-account-link">Account</Link><br/>
-      <Link href="/Dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
-      <Link href="/Dashboard" id="wd-course-link">Courses</Link><br/>
-      <Link href="/Calendar" id="wd-calendar-link">Calendar</Link><br/>
-      <Link href="/Inbox" id="wd-inbox-link">Inbox</Link><br/>
-      <Link href="/Labs" id="wd-labs-link">Labs</Link><br/>
-    </div>
-);}
+    <ListGroup
+      className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
+      style={{ width: 110 }}        
+      id="wd-kambaz-navigation">
+      <ListGroupItem className="bg-black border-0 text-center" as="a"
+        target="_blank"
+        href="https://www.northeastern.edu/"
+        id="wd-neu-link">
+        <img src="/images/NU_CMYK_Notched-N_motto_RW.png" width="75px" alt="Northeastern University" />
+      </ListGroupItem>
+
+      {/* Account */}
+      <ListGroupItem className={`border-0 text-center ${isActive("/Account") ? "bg-white" : "bg-black"}`}>
+        <Link href="/Account" id="wd-account-link"
+          className={`${isActive("/Account") ? "text-danger" : "text-white"} text-decoration-none`}>
+            <FaRegCircleUser className="fs-1 text-white" /> 
+            <br/>
+            Account
+        </Link>
+      </ListGroupItem>
+
+      {/* Dashboard */}
+      <ListGroupItem className={`border-0 text-center ${isActive("/Dashboard") ? "bg-white" : "bg-black"}`}>
+        <Link href="/Dashboard" id="wd-dashboard-link"
+          className={`${isActive("/Dashboard") ? "text-danger" : "text-white"} text-decoration-none`}>
+            <AiOutlineDashboard className="fs-1 text-danger" /> 
+            <br />
+            Dashboard
+        </Link>
+      </ListGroupItem>
+
+      {/* Courses */}
+      <ListGroupItem className={`border-0 text-center ${isActive("/Dashboard") ? "bg-white" : "bg-black"}`}>
+        <Link href="/Dashboard" id="wd-course-link"
+          className={`${isActive("/Dashboard") ? "text-danger" : "text-white"} text-decoration-none`}>
+            <LiaBookSolid className="fs-1 text-danger" />
+            <br />
+            Courses
+        </Link>
+      </ListGroupItem>
+
+      {/* Calendar */}
+      <ListGroupItem className={`border-0 text-center ${isActive("/Calendar") ? "bg-white" : "bg-black"}`}>
+        <Link href="/Calendar" id="wd-calendar-link"
+          className={`${isActive("/Calendar") ? "text-danger" : "text-white"} text-decoration-none`}>
+            <IoCalendarOutline className="fs-1 text-danger" />
+            <br />
+            Calendar
+        </Link>
+      </ListGroupItem>
+  
+      {/* Inbox */}
+      <ListGroupItem className={`border-0 text-center ${isActive("/Inbox") ? "bg-white" : "bg-black"}`}>
+        <Link href="/Inbox" id="wd-inbox-link"
+          className={`${isActive("/Inbox") ? "text-danger" : "text-white"} text-decoration-none`}>
+            <FaInbox className="fs-1 text-danger"/>
+            <br/>
+            Inbox
+        </Link>
+      </ListGroupItem> 
+
+      {/* Labs */}
+      <ListGroupItem className={`border-0 ${isActive("/Labs") ? "bg-white" : "bg-black"} text-center`}>
+        <Link href="/Labs" id="wd-labs-link"
+          className={`${isActive("/Labs") ? "text-danger" : "text-white"} text-decoration-none`}>
+            <LiaCogSolid className="fs-1 text-danger" />
+            <br />
+            Labs
+        </Link>
+      </ListGroupItem>
+      <br />
+    </ListGroup>
+  );
+}
+
