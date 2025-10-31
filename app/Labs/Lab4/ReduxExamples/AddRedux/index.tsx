a683a1766aed6a9a31eb45f9250166ba6b3e0347
migchi430 <chica.m@northeastern.edu>
2025-10-31 19:02:15 -0400

    fix

--- app/Labs/Lab4/ReduxExamples/AddRedux/index.tsx
+++ app/Labs/Lab4/ReduxExamples/AddRedux/index.tsx
@@ -1,5 +1,7 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
 import { add } from "./addReducer";
